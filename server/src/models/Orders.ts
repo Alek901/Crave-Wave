419fd052da665e0f0cefcd8c89e7fc97507c7740
keshon16 <keshonmckie16@gmail.com>
2024-12-09 22:38:26 -0500

    Update Orders.ts

--- server/src/models/Orders.ts
+++ server/src/models/Orders.ts
@@ -1,28 +1,18 @@
-import {Schema, model, type document} from 'mongoose';
+import {Schema, model, type Document} from 'mongoose';
 
 export interface IOrder extends Document {
-    orderId: string;
     name: string;
-    
+    order: string;
 }
+const OrderSchema = new Schema<IOrder>({
+    orderId: { type: String },
+    userId: { type: String },
+    description: { type: String },
+    billing_price: { type: Number },
+    total_price: { type: Number },
+    status: { type: String },
+    billing_address: { type: String }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const Order = model('Order', orderSchema); 
+const Order = model('Order', OrderSchema); 
 export default { Order };
