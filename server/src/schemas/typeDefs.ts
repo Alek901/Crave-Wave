c410ee9c73c7a7bf3841779b0224462dce4a705d
Kevin Perez <kevin_perez14@comcast.net>
2024-12-11 20:53:13 -0500

    LOTS of changes

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,20 +1,42 @@
 const typeDefs = `
-  type Tech {
+  type Meals {
     _id: ID!
     name: String!
+    ingredients: String!
+    price: Int
+    combo: String!
   }
 
-  type Matchup {
+  type Delivery {
     _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
+   customer_id: Int 
+   order_id: Int
+   meal_id: Int
+   delivery_time: String!
+   delivery_address: String!
+   delivery_status: String!
+   delivery_price: Int
+   delivery_data: String!
+  }
+
+  type Restaurant {
+    _id: ID!
+    name: String!
+    menu_description: String!
+    address: String!
+    phone_number: Int
+    email: String!
+    meal_id: Int
+    order_id: Int
+    menu_id: Int
+    menu_options: String!
+    menu_price: Int
   }
 
   type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+    meals: [Meals]
+    delivery: [Delivery]
+    restaurant: [Restaurant]
   }
 
   type Mutation {
