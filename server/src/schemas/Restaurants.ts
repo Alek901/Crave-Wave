03449ccae70092badf7a0d50aa2eb4a8278d79aa
keshon16 <keshonmckie16@gmail.com>
2024-12-12 01:29:10 -0500

    Update Restaurants.ts

--- server/src/schemas/Restaurants.ts
+++ server/src/schemas/Restaurants.ts
@@ -1,9 +1,24 @@
-import Meal, {IMeal} from '../models/Meals.js';
-import Order, { IOrder } from '../models/Orders.js';
+import  { Restaurant } from "../models/Restaurant"
 
-
-const restaurants = {
-    getAllMeals: async () => {
-        
-    }
+const restaurantsResolvers = {
+   Query: {
+        restaurants: async (): Promise<Restaurant[] | null> => {
+            return Restaurant.find({})
+        },
+        restaurant: async (_parent: any, { id }: { id: string }): Promise<Restaurant | null> => {
+            return await Restaurant.findById(id)
+        },    },
+    Mutation: {
+        createRestaurant: async (_parent: any, { input }: { input: any }): Promise<Restaurant> => {
+            const restaurant = await Restaurant.create(input)
+            return restaurant
+        }    },
+    updateRestaurant: async (_parent: any, args: any): Promise<Restaurant | null> => {
+        const restaurant = await Restaurant.findByIdAndUpdate(id, input, { new: true })
+    },
 }
+
+
+export default restaurantsResolvers
+
+    
