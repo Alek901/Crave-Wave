e261980a7ced4b543eb345dff99ac99a15513d30
Kevin Perez <kevin_perez14@comcast.net>
2024-12-12 19:54:59 -0500

    Updated index, new mealData file, and updated seeds. Seeds still has errors at the moment

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,5 +1,6 @@
 import Delivery from './Delivery.js';
 import Meals from './Meals.js';
 import Restaurant from './Restaurant.js';
+import Order from './Orders.js';
 
-export default { Delivery, Meals, Restaurant };
+export default { Delivery, Meals, Restaurant, Order };
