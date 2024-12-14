ee11fab49dc56fd82f2c0f6f45735fc2370c56aa
keshon16 <keshonmckie16@gmail.com>
2024-12-14 00:21:30 -0500

    Updated the schemas and added User.ts

--- server/src/schemas/Orders.ts
+++ server/src/schemas/Orders.ts
@@ -8,7 +8,20 @@ cosnt OrdersResolvers = {
         orders: async (): Promise<OrdersType[] | null> => {
             return await Orders.find({});
     },
-    order: async (_parent: any, { id }: { id: string}): Promise<
+    order: async (_parent: any, { id }: { id: string}): Promise<OrdersType | null> => {
+        return await Orders.findById(id);
+    },
+},
+Mutation: {
+    createOrder: async (_parent: any, { input }: { input: any }): Promise<OrdersType> => {
+        const newOrder = await Orders.create(input);
+        return newOrder;
+    },
+    updateOrder: async (_parent: any, { id, input }: { id: string; input: any }): Promise<OrdersType | null> => {
+        return await Orders.findByIdAndUpdate(id, input, { new: true });
+    },
+},
 }
 
-}
+export default OrdersResolvers;
+
---
+++ server/src/schemas/Users.ts
@@ -0,0 +1,25 @@
+import mongoose, { InferSchemaType } from "mongoose";
+import User from "../models/Users"
+
+type UserType = InferSchemaType<typeof User.schema>;
+
+
+const usersResolvers = {
+    Query: {
+        users: async (): Promise<UserType[] | null> => {
+            return await User.find({});
+        },
+        user: async (_parent: any, { id }: { id: string }): Promise<UserType | null> => {
+            return await User.findById(id);
+        },
+    },
+    Mutation: {
+        createUser: async (_parent: any, { input }: { input: any }): Promise<UserType> => {
+            const newUser = await User.create(input);
+            return newUser;
+        },
+        updateUser: async (_parent: any, { id, input }: { id: string; input: any }): Promise<UserType | null> => {
+            return await User.findByIdAndUpdate(id, input, { new: true });
+        },
+    },
+};
