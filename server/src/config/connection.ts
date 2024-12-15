3dd0796a50502b0e81d3455ff40d3cf90cee8d91
Bugatti1982 <welch8119@gmail.com>
2024-12-14 19:47:03 -0600

    Made changes to server.ts and got rid of unnecessary comments

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -16,24 +16,3 @@ async function db() {
 }
 
 export default db;
-
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// import mongoose from 'mongoose';
-
-// const MONGODB_URI = process.env.MONGODB_URI || '';
-
-// const db = async (): Promise<typeof mongoose.connection> => {
-//   console.log(MONGODB_URI)
-//   try {
-//     await mongoose.connect(MONGODB_URI);
-//     console.log('Database connected.');
-//     return mongoose.connection;
-//   } catch (error) {
-//     console.error('Database connection error:', error);
-//     throw new Error('Database connection failed.');
-//   }
-// };
-
-// export default db;
